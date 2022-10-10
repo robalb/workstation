b6a71d5e8355e540425b4029bc632330fbc758ea
robalb <notmyreal@mail.mail>
2022-10-11 00:18:00 +0200

    fixed logic issues

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ extra=true
 
 # install a package from apt default repositories if not already installed
 apt_requirement(){
-  type -p $1 >/dev/null || echo -e "${COLOR} $1 not found, installing it ${NC}" &&\
-    sudo apt -qq install $1 -y
+  type -p $1 >/dev/null \
+    || (echo -e "${COLOR} $1 not found, installing it ${NC}" \
+    && sudo apt -qq install $1 -y )
 }
 
 echo -e "${COLOR} ====== [core] SETUP =======${NC}"
@@ -18,12 +19,12 @@ apt_requirement git
 sudo apt install software-properties-common -y
 
 #install gh (github cli) if not already installed
-type -p gh >/dev/null || echo -e "${COLOR} gh not found, installing it ${NC}" \
+type -p gh >/dev/null || (echo -e "${COLOR} gh not found, installing it ${NC}" \
   && curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
   && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
   && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
   && sudo apt update \
-  && sudo apt install gh -y
+  && sudo apt install gh -y )
 
 
 echo -e "${COLOR} ====== [core] GITHUB AUTH =======${NC}"
@@ -38,16 +39,17 @@ apt_requirement xclip
 # commented out, this is hardcoded in the dotfiles repo now
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #install neovim if not already installed
-type -p nvim >/dev/null || echo -e "${COLOR} neovim not found, installing it ${NC}" &&\
+type -p nvim >/dev/null || (echo -e "${COLOR} neovim not found, installing it ${NC}" &&\
   sudo add-apt-repository ppa:neovim-ppa/stable -y &&\
   sudo apt-get update &&\
-  sudo apt -qq install neovim -y
+  sudo apt -qq install neovim -y )
 
 echo -e "${COLOR} ====== [core] DOTFILES SETUP =======${NC}"
 # https://www.ackama.com/what-we-think/the-best-way-to-store-your-dotfiles-a-bare-git-repository-explained/
 # Initialize the bare repo and pull the dotfiles from the origin.
 # All these operations are idempotent
-[ -d "$HOME/.cfg" ] && echo -e "${COLOR} ====== [core] skipping dotfiles setup =======${NC}" || ( echo -e "${COLOR} ====== [core] DOTFILES SETUP =======${NC}" \
+[ -d "$HOME/.cfg" ] && echo -e "${COLOR} ====== [core] skipping dotfiles setup =======${NC}" \
+  || ( echo -e "${COLOR} ====== [core] DOTFILES SETUP =======${NC}" \
   && git init --bare $HOME/.cfg \
   && git --git-dir=$HOME/.cfg/ --work-tree=$HOME remote add origin https://github.com/robalb/dotfiles \
   && git --git-dir=$HOME/.cfg/ --work-tree=$HOME pull --ff-only origin master )
